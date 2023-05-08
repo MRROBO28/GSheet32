42cea43e69e4b47300bac18f8ea91595a1010d50
MRROBO28 <irshadfaqih113@gmail.com>
2023-05-08 18:20:05 +0700

    penambahan fitur baru

--- GSheet32.cpp
+++ GSheet32.cpp
@@ -22,7 +22,7 @@ void GSheet32::connectWiFi(const char* ssid, const char* pass){
     Serial.print("Succes!! Connect to : "+WiFi.SSID());
 }
 
-void GSheet32::sendData(String param){
+void GSheet32::sendData(String colC, String colD, String colE, String colF, String colG){
     String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?"+param;
     http.begin(url.c_str()); //Specify the URL and certificate
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);

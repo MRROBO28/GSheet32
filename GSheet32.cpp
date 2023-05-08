ae6068ec6c05ddde627e23248ca798fba58dd2ae
MRROBO28 <irshadfaqih113@gmail.com>
2023-05-08 18:26:48 +0700

    penambahan fitur baru

--- GSheet32.cpp
+++ GSheet32.cpp
@@ -22,8 +22,76 @@ void GSheet32::connectWiFi(const char* ssid, const char* pass){
     Serial.print("Succes!! Connect to : "+WiFi.SSID());
 }
 
-void GSheet32::sendData(String colC, String colD, String colE, String colF, String colG){
-    String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?"+param;
+void GSheet32::sendData(String colC){
+    String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?value1="+colC;
+    http.begin(url.c_str()); //Specify the URL and certificate
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    int httpCode = http.GET();
+    String payload;
+    if (httpCode > 0){
+      payload = http.getString();
+     Serial.println(httpCode);
+     Serial.println(payload);
+     }else {
+     Serial.println("Failed");
+    }
+        
+     http.end();
+}
+
+void GSheet32::sendData(String colC, String colD){
+    String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?value1="+colC+"&value2="+colD;
+    http.begin(url.c_str()); //Specify the URL and certificate
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    int httpCode = http.GET();
+    String payload;
+    if (httpCode > 0){
+      payload = http.getString();
+     Serial.println(httpCode);
+     Serial.println(payload);
+     }else {
+     Serial.println("Failed");
+    }
+        
+     http.end();
+}
+
+void GSheet32::sendData(String colC, String colD,String colE){
+    String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?value1="+colC+"&value2="+colD+"&value3="+colE;
+    http.begin(url.c_str()); //Specify the URL and certificate
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    int httpCode = http.GET();
+    String payload;
+    if (httpCode > 0){
+      payload = http.getString();
+     Serial.println(httpCode);
+     Serial.println(payload);
+     }else {
+     Serial.println("Failed");
+    }
+        
+     http.end();
+}
+
+void GSheet32::sendData(String colC, String colD,String colE, String colF){
+    String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?value1="+colC+"&value2="+colD+"&value3="+colE+"&value4="+colF;
+    http.begin(url.c_str()); //Specify the URL and certificate
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    int httpCode = http.GET();
+    String payload;
+    if (httpCode > 0){
+      payload = http.getString();
+     Serial.println(httpCode);
+     Serial.println(payload);
+     }else {
+     Serial.println("Failed");
+    }
+        
+     http.end();
+}
+
+void GSheet32::sendData(String colC, String colD,String colE, String colF, String colG){
+    String url = "https://script.google.com/macros/s/" + _GAS_ID + "/exec?value1="+colC+"&value2="+colD+"&value3="+colE+"&value4="+colF+"&value5="+colG;
     http.begin(url.c_str()); //Specify the URL and certificate
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
     int httpCode = http.GET();
